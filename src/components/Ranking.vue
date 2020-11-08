a7115531adb8c8c87b8472391cd584ba98929901
ihgs <hgs.dev@gmail.com>
2020-11-08 23:28:04 +0900

    Add keys

---
+++ src/components/Ranking.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="ranking">
+    <b-container>
+      <div v-if="!edit_title">
+        <div>{{ title }}</div>
+        <b-link v-on:click="switch_edit_title">edit</b-link>
+      </div>
+      <div v-else>
+        <b-form-input
+          v-model="title"
+          type="text"
+          placeholder="Enter Title"
+          v-if="edit_title"
+        ></b-form-input>
+        <b-link v-on:click="switch_edit_title">save</b-link>
+      </div>
+      <div>
+        <b-form-input v-model="key"></b-form-input
+        ><b-button v-on:click="add_key">Add</b-button>
+        <b-table striped hover :fields="fields" :items="keys">
+          <template #cell(actions)="row">
+            <b-icon-arrow-up v-on:click="move(row.index, -1)"></b-icon-arrow-up>
+            <b-icon-arrow-down
+              v-on:click="move(row.index, 1)"
+            ></b-icon-arrow-down>
+            <b-icon-trash v-on:click="remove(row.index)"></b-icon-trash>
+          </template>
+        </b-table>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ranking",
+  data: function() {
+    return {
+      fields: ["key", "actions"],
+      title: "Test",
+      edit_title: false,
+      keys: [{ key: "aaa" }, { key: "bbb" }],
+      key: ""
+    };
+  },
+  methods: {
+    switch_edit_title: function() {
+      this.edit_title = !this.edit_title;
+    },
+    add_key: function() {
+      this.keys.push({
+        key: this.key
+      });
+      this.key = "";
+    },
+    move: function(index, diff) {
+      const targetId = index + diff;
+      if (targetId < 0 || targetId >= this.keys.length) {
+        return;
+      }
+      const sourceId = index;
+      const cloneArray = [...this.keys];
+      [cloneArray[targetId], cloneArray[sourceId]] = [
+        this.keys[sourceId],
+        this.keys[targetId]
+      ];
+      this.keys = cloneArray;
+      return;
+    },
+    remove: function(index) {
+      const cloneArray = [...this.keys];
+      cloneArray.splice(index, 1);
+      this.keys = cloneArray;
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Ranking />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Ranking from "@/components/Ranking.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Ranking
   }
 };
 </script>
