c2bd1bc45ab771f0440c935a154fe11cc47e8dc6
ihgs <hgs.dev@gmail.com>
2020-11-11 18:45:54 +0900

    enable to show graph

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -67,12 +67,14 @@
           </template>
         </b-table>
         <b-button v-on:click="show_graph">Shwo graph</b-button>
+        <div id="viz"></div>
       </div>
     </b-container>
   </div>
 </template>
 
 <script>
+import embed from "vega-embed";
 export default {
   name: "Ranking",
   data: function() {
@@ -85,7 +87,23 @@ export default {
       key: "",
       targets: [{ target: "AAAA" }, { target: "CCCC" }, { target: "DDDD" }],
       target: "",
-      contents: {"aaa":{"AAAA":{"point":"1","description":"a"},"CCCC":{"point":"2","description":"b"},"DDDD":{"point":"3","description":"c"}},"bbb":{"AAAA":{"point":"4","description":"d"},"CCCC":{"point":"5","description":"e"},"DDDD":{"point":"6","description":"f"}}}
+      contents: {
+        aaa: {
+          AAAA: { point: "1", description: "a" },
+          CCCC: { point: "2", description: "b" },
+          DDDD: { point: "3", description: "c" }
+        },
+        bbb: {
+          AAAA: { point: "4", description: "d" },
+          CCCC: { point: "5", description: "e" },
+          DDDD: { point: "6", description: "f" }
+        },
+        CCCC: {
+          AAAA: { point: "9", description: "g" },
+          CCCC: { point: "8", description: "h" },
+          DDDD: { point: "7", description: "i" }
+        }
+      }
     };
   },
   computed: {
@@ -175,6 +193,206 @@ export default {
     },
     show_graph: function() {
       console.log(this.contents);
+      const data = [];
+      Object.keys(this.contents).forEach(k => {
+        Object.keys(this.contents[k]).forEach(t => {
+          const datum = {
+            key: k,
+            category: t,
+            value: this.contents[k][t].point,
+            description: this.contents[k][t].description
+          };
+
+          data.push(datum);
+        });
+      });
+      this.draw(data);
+    },
+    async draw(data) {
+      console.log(data);
+      let def = {
+        $schema: "https://vega.github.io/schema/vega/v5.json",
+        description:
+          "A radar chart example, showing multiple dimensions in a radial layout.",
+        width: 400,
+        height: 400,
+        padding: 40,
+        autosize: { type: "none", contains: "padding" },
+
+        signals: [{ name: "radius", update: "width / 2" }],
+
+        data: [
+          {
+            name: "table",
+            values: data
+          },
+          {
+            name: "keys",
+            source: "table",
+            transform: [
+              {
+                type: "aggregate",
+                groupby: ["key"]
+              }
+            ]
+          }
+        ],
+
+        scales: [
+          {
+            name: "angular",
+            type: "point",
+            range: { signal: "[-PI, PI]" },
+            padding: 0.5,
+            domain: { data: "table", field: "key" }
+          },
+          {
+            name: "radial",
+            type: "linear",
+            range: { signal: "[0, radius]" },
+            zero: true,
+            nice: false,
+            domain: { data: "table", field: "value" },
+            domainMin: 0
+          },
+          {
+            name: "color",
+            type: "ordinal",
+            domain: { data: "table", field: "category" },
+            range: { scheme: "category10" }
+          }
+        ],
+
+        encode: {
+          enter: {
+            x: { signal: "radius" },
+            y: { signal: "radius" }
+          }
+        },
+
+        marks: [
+          {
+            type: "group",
+            name: "categories",
+            zindex: 1,
+            from: {
+              facet: { data: "table", name: "facet", groupby: ["category"] }
+            },
+            marks: [
+              {
+                type: "line",
+                name: "category-line",
+                from: { data: "facet" },
+                encode: {
+                  enter: {
+                    interpolate: { value: "linear-closed" },
+                    x: {
+                      signal:
+                        "scale('radial', datum.value) * cos(scale('angular', datum.key))"
+                    },
+                    y: {
+                      signal:
+                        "scale('radial', datum.value) * sin(scale('angular', datum.key))"
+                    },
+                    stroke: { scale: "color", field: "category" },
+                    strokeWidth: { value: 1 },
+                    fill: { scale: "color", field: "category" },
+                    fillOpacity: { value: 0.1 }
+                  }
+                }
+              },
+              {
+                type: "text",
+                name: "value-text",
+                from: { data: "category-line" },
+                encode: {
+                  enter: {
+                    x: { signal: "datum.x" },
+                    y: { signal: "datum.y" },
+                    text: { signal: "datum.datum.value" },
+                    align: { value: "center" },
+                    baseline: { value: "middle" },
+                    fill: { value: "black" },
+                    tooltip: { signal: "datum.datum.description" }
+                  }
+                }
+              }
+            ]
+          },
+          {
+            type: "rule",
+            name: "radial-grid",
+            from: { data: "keys" },
+            zindex: 0,
+            encode: {
+              enter: {
+                x: { value: 0 },
+                y: { value: 0 },
+                x2: { signal: "radius * cos(scale('angular', datum.key))" },
+                y2: { signal: "radius * sin(scale('angular', datum.key))" },
+                stroke: { value: "lightgray" },
+                strokeWidth: { value: 1 }
+              }
+            }
+          },
+          {
+            type: "text",
+            name: "key-label",
+            from: { data: "keys" },
+            zindex: 1,
+            encode: {
+              enter: {
+                x: {
+                  signal: "(radius + 5) * cos(scale('angular', datum.key))"
+                },
+                y: {
+                  signal: "(radius + 5) * sin(scale('angular', datum.key))"
+                },
+                text: { field: "key" },
+                align: [
+                  {
+                    test: "abs(scale('angular', datum.key)) > PI / 2",
+                    value: "right"
+                  },
+                  {
+                    value: "left"
+                  }
+                ],
+                baseline: [
+                  {
+                    test: "scale('angular', datum.key) > 0",
+                    value: "top"
+                  },
+                  {
+                    test: "scale('angular', datum.key) == 0",
+                    value: "middle"
+                  },
+                  {
+                    value: "bottom"
+                  }
+                ],
+                fill: { value: "black" },
+                fontWeight: { value: "bold" }
+              }
+            }
+          },
+          {
+            type: "line",
+            name: "outer-line",
+            from: { data: "radial-grid" },
+            encode: {
+              enter: {
+                interpolate: { value: "linear-closed" },
+                x: { field: "x2" },
+                y: { field: "y2" },
+                stroke: { value: "lightgray" },
+                strokeWidth: { value: 1 }
+              }
+            }
+          }
+        ]
+      };
+      await embed("#viz", def, { actions: false });
     }
   },
   watch: {
