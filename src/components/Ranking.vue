17c08767909b157651050dea8894ff6e42caf0f2
ihgs <hgs.dev@gmail.com>
2020-11-23 17:51:21 +0900

    refactoring

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -100,7 +100,9 @@
 
 <script>
 import embed from "vega-embed";
-import { v4 as uuidv4 } from "uuid";
+import {spec as radar} from './scheam/radar';
+import MyStorage from '../store/storage';
+const storage = new MyStorage();
 export default {
   name: "Ranking",
   data: function() {
@@ -119,10 +121,7 @@ export default {
     };
   },
   created: function() {
-    const savedData = localStorage.getItem("default");
-    if (savedData) {
-      this.load(savedData);
-    }
+
   },
   computed: {
     ranking_header: function() {
@@ -218,7 +217,7 @@ export default {
       return "cell(" + field + ")";
     },
     refresh_list: function() {
-      const savedData = JSON.parse(localStorage.getItem("myranking:uuids"));
+      const savedData = storage.uuids();
       this.list_saved_data = [];
       if (savedData) {
         Object.keys(savedData).forEach(k => {
@@ -249,8 +248,8 @@ export default {
         alert("選択してください");
         return;
       }
-      const selected_uuid = this.selected[0].uuid;
-      const data = localStorage.getItem("myranking:data:" + selected_uuid);
+      const data = storage.get(this.selected[0].uuid);
+      this.uuid = this.selected[0].uuid;
       this.load(data);
     },
     save: function() {
@@ -259,22 +258,13 @@ export default {
         contents: this.contents
       };
       if (!this.uuid) {
-        this.uuid = uuidv4();
+        this.uuid = storage.create(saveData);
+      }else {
+        storage.save(this.uuid, saveData);
       }
-      localStorage.setItem(
-        "myranking:data:" + this.uuid,
-        JSON.stringify(saveData)
-      );
-      let uuids = JSON.parse(localStorage.getItem("myranking:uuids"));
-      if (!uuids) {
-        uuids = {};
-      }
-      uuids[this.uuid] = this.title;
-      localStorage.setItem("myranking:uuids", JSON.stringify(uuids));
       alert("保存しました");
     },
     show_graph: function() {
-      console.log(this.contents);
       const data = [];
       Object.keys(this.contents).forEach(k => {
         Object.keys(this.contents[k]).forEach(t => {
@@ -291,189 +281,7 @@ export default {
       this.draw(data);
     },
     async draw(data) {
-      console.log(data);
-      let def = {
-        $schema: "https://vega.github.io/schema/vega/v5.json",
-        description:
-          "A radar chart example, showing multiple dimensions in a radial layout.",
-        width: 400,
-        height: 400,
-        padding: 40,
-        autosize: { type: "none", contains: "padding" },
-
-        signals: [{ name: "radius", update: "width / 2" }],
-
-        data: [
-          {
-            name: "table",
-            values: data
-          },
-          {
-            name: "keys",
-            source: "table",
-            transform: [
-              {
-                type: "aggregate",
-                groupby: ["key"]
-              }
-            ]
-          }
-        ],
-
-        scales: [
-          {
-            name: "angular",
-            type: "point",
-            range: { signal: "[-PI, PI]" },
-            padding: 0.5,
-            domain: { data: "table", field: "key" }
-          },
-          {
-            name: "radial",
-            type: "linear",
-            range: { signal: "[0, radius]" },
-            zero: true,
-            nice: false,
-            domain: { data: "table", field: "value" },
-            domainMin: 0
-          },
-          {
-            name: "color",
-            type: "ordinal",
-            domain: { data: "table", field: "category" },
-            range: { scheme: "category10" }
-          }
-        ],
-
-        encode: {
-          enter: {
-            x: { signal: "radius" },
-            y: { signal: "radius" }
-          }
-        },
-
-        marks: [
-          {
-            type: "group",
-            name: "categories",
-            zindex: 1,
-            from: {
-              facet: { data: "table", name: "facet", groupby: ["category"] }
-            },
-            marks: [
-              {
-                type: "line",
-                name: "category-line",
-                from: { data: "facet" },
-                encode: {
-                  enter: {
-                    interpolate: { value: "linear-closed" },
-                    x: {
-                      signal:
-                        "scale('radial', datum.value) * cos(scale('angular', datum.key))"
-                    },
-                    y: {
-                      signal:
-                        "scale('radial', datum.value) * sin(scale('angular', datum.key))"
-                    },
-                    stroke: { scale: "color", field: "category" },
-                    strokeWidth: { value: 1 },
-                    fill: { scale: "color", field: "category" },
-                    fillOpacity: { value: 0.1 }
-                  }
-                }
-              },
-              {
-                type: "text",
-                name: "value-text",
-                from: { data: "category-line" },
-                encode: {
-                  enter: {
-                    x: { signal: "datum.x" },
-                    y: { signal: "datum.y" },
-                    text: { signal: "datum.datum.value" },
-                    align: { value: "center" },
-                    baseline: { value: "middle" },
-                    fill: { value: "black" },
-                    tooltip: { signal: "datum.datum.description" }
-                  }
-                }
-              }
-            ]
-          },
-          {
-            type: "rule",
-            name: "radial-grid",
-            from: { data: "keys" },
-            zindex: 0,
-            encode: {
-              enter: {
-                x: { value: 0 },
-                y: { value: 0 },
-                x2: { signal: "radius * cos(scale('angular', datum.key))" },
-                y2: { signal: "radius * sin(scale('angular', datum.key))" },
-                stroke: { value: "lightgray" },
-                strokeWidth: { value: 1 }
-              }
-            }
-          },
-          {
-            type: "text",
-            name: "key-label",
-            from: { data: "keys" },
-            zindex: 1,
-            encode: {
-              enter: {
-                x: {
-                  signal: "(radius + 5) * cos(scale('angular', datum.key))"
-                },
-                y: {
-                  signal: "(radius + 5) * sin(scale('angular', datum.key))"
-                },
-                text: { field: "key" },
-                align: [
-                  {
-                    test: "abs(scale('angular', datum.key)) > PI / 2",
-                    value: "right"
-                  },
-                  {
-                    value: "left"
-                  }
-                ],
-                baseline: [
-                  {
-                    test: "scale('angular', datum.key) > 0",
-                    value: "top"
-                  },
-                  {
-                    test: "scale('angular', datum.key) == 0",
-                    value: "middle"
-                  },
-                  {
-                    value: "bottom"
-                  }
-                ],
-                fill: { value: "black" },
-                fontWeight: { value: "bold" }
-              }
-            }
-          },
-          {
-            type: "line",
-            name: "outer-line",
-            from: { data: "radial-grid" },
-            encode: {
-              enter: {
-                interpolate: { value: "linear-closed" },
-                x: { field: "x2" },
-                y: { field: "y2" },
-                stroke: { value: "lightgray" },
-                strokeWidth: { value: 1 }
-              }
-            }
-          }
-        ]
-      };
+      let def = radar(data)
       await embed("#viz", def, { actions: false });
     }
   }
---
+++ src/store/storage.js
@@ -0,0 +1,37 @@
+import { v4 as uuidv4 } from "uuid";
+
+export default class MyStorage {
+
+  constructor () {
+
+  }
+
+  create(data){
+    const uuid = uuidv4();
+    this.save(uuid, data)
+    return uuid;
+  }
+
+  save(uuid, data){
+    localStorage.setItem(
+      "myranking:data:" + uuid,
+      JSON.stringify(data)
+    );
+    let uuids = this.uuids();
+    uuids[uuid] = data.title;
+    localStorage.setItem("myranking:uuids", JSON.stringify(uuids));
+    return;
+  }
+  uuids(){
+    let uuids = JSON.parse(localStorage.getItem("myranking:uuids"));
+    if (!uuids) {
+      uuids = {};
+    }
+    return uuids;
+  }
+
+  get(uuid){
+    return localStorage.getItem("myranking:data:" + uuid);;
+  }
+
+}
